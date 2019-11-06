820c7235a20bcddd7f632b53bfe48f7568506595
dalia <dalia.91a@gmail.com>
2019-11-06 16:14:09 +0300

    Updates

---
+++ precache-manifest.3bd3011f1dd2c89157d08d05f741b3a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb33201b71d7b619f3831152bf68c715",
+    "url": "/books/index.html"
+  },
+  {
+    "revision": "dd3b60746af80444f132",
+    "url": "/books/static/css/main.a4ef1de9.chunk.css"
+  },
+  {
+    "revision": "c376197460f98f253741",
+    "url": "/books/static/js/2.e484458d.chunk.js"
+  },
+  {
+    "revision": "dd3b60746af80444f132",
+    "url": "/books/static/js/main.ea5553df.chunk.js"
+  },
+  {
+    "revision": "f52eaa421f57873dbf1c",
+    "url": "/books/static/js/runtime-main.8b992ffc.js"
+  },
+  {
+    "revision": "c6fe4c3ff0928651f51bd283220dba5c",
+    "url": "/books/static/media/boxx.c6fe4c3f.svg"
+  },
+  {
+    "revision": "deda33aea2bfa9216df4b9abc2d17698",
+    "url": "/books/static/media/gmail.deda33ae.svg"
+  }
+]);
